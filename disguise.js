1fbe20394109ae8c7229c54c9a74500d3a22dcd0
tleilax <tleilax+github@gmail.com>
2012-03-27 11:20:27 +0000

    v1.4.1: - Bugfix if no search results were found

--- disguise.js
+++
@@ -1,6 +0,0 @@
-jQuery(function ($) {	
-	$('<a />').attr('href', '<?= $link ?>')
-		.text("Als dieser Nutzer einloggen".toLocaleString())
-		.before('<br/>')
-		.insertAfter('#layout_container > table:first td:first > :not(br):last');
-});
