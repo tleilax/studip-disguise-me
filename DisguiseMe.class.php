336711cd10782fc1642b5fbc77f337d975102939
Jan-Hendrik Willms <tleilax+github@gmail.com>
2015-05-07 09:53:44 +0200

    introduce stealth mode: the adopted user will not show as online

--- DisguiseMe.class.php
+++ DisguiseMe.class.php
@@ -1,36 +1,18 @@
 <?php
-// +---------------------------------------------------------------------------+
-// DisguiseMe.class.php
-//
-// Copyright (c) 2012 Jan-Hendrik Willms <tleilax+studip@gmail.com>
-// +---------------------------------------------------------------------------+
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or any later version.
-// +---------------------------------------------------------------------------+
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-// +---------------------------------------------------------------------------+/
-
-
 /**
  * DisguiseMe.class.php
  *
- * @author      Jan-Hendrik Willms <tleilax+studip@gmail.com>
- * @package     IBIT_StudIP
- * @version     1.3.1
+ * @author   Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @package  IBIT_StudIP
+ * @version  2.1
+ * @license  GPL2 or any later version
  */
 class DisguiseMe extends StudIPPlugin implements SystemPlugin
 {
     private static $hit_once = false;
 
-    public function getPluginname() {
+    public function getPluginname()
+    {
         return _('Disguise Me');
     }
 
@@ -63,6 +45,16 @@ class DisguiseMe extends StudIPPlugin implements SystemPlugin
             ));
             PageLayout::addHeadElement('script', array(), $script);
         }
+
+        // Improvement by anoack: Hide activity when disguised so the user
+        // won't show as online
+        $stealth_mode = function () {
+            if (is_object($GLOBALS['sess'])) {
+                @session_write_close();
+            }
+            throw new NotificationVetoException();
+        };
+        NotificationCenter::addObserver($stealth_mode, '__invoke', 'PageCloseWillExecute');
     }
 
     public function perform($unconsumed_path)
--- disguise-search-js.php
+++ disguise-search-js.php
@@ -3,7 +3,7 @@ jQuery(function ($) {
         $('#layout_content form:last tr:not(:first):not(:last)').each(function () {
             var username = $.trim($('td:eq(1) a', this).text()),
                 link = '<?= $link ?>'.replace('REPLACE-WITH-USER', username);
-            $('<a title="Als dieser Nutzer einloggen"/>').attr('href', link)
+            $('<a title="<?= _('Als dieser Nutzer einloggen') ?>"/>').attr('href', link)
                 .html('<?= Assets::img('icons/16/red/door-enter.png') ?>')
                 .prependTo( $('td:last', this) );
         });
