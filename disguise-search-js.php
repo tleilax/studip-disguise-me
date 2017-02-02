a09778ae753db12ef217fa8ccb76ca25d1f3477b
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-02-02 13:57:04 +0100

    disallow disguising as blocked users, fixes #5

--- disguise-search-js.php
+++ disguise-search-js.php
@@ -1,6 +1,10 @@
 jQuery(function ($) {
     if ($('#layout_content form table').length > 1) {
         $('#layout_content form:last tr:not(:first):not(:last)').each(function () {
+            if ($(this).html().toLowerCase().indexOf('gesperrt') !== -1) {
+                return;
+            }
+
             var username = $.trim($('td:eq(1) a', this).text()),
                 link = '<?= $link ?>'.replace('REPLACE-WITH-USER', username);
             $('<a title="<?= _('Als dieser Nutzer einloggen') ?>"/>').attr('href', link)
